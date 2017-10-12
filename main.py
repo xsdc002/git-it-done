2e499681b917c00fff2f4f47e1126d213c46a6d0
unknown <xsdc002@gmail.com>
2017-10-12 13:13:15 -0500

    error commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get.it.done:get.it.done@localhost:3306/get.it.done'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 tasks = []
 
@@ -12,7 +26,8 @@ def index():
         task = request.form['task']
         tasks.append(task)
 
-    return render_template('todos.html',title="Get It Done!", tasks=tasks)
+    return render_template('todos.html', title="Get It Done!", tasks=tasks)
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
